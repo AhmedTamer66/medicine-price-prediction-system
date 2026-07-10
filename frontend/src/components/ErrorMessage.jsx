9915d5e42ea45861375fdfa83164d921f2047b2a
Haitham <52435658+alacksch@users.noreply.github.com>
2026-07-10 16:32:09 +0300

    Delete frontend directory

--- frontend/src/components/ErrorMessage.jsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorMessage({ message }) {
-  return (
-    <div className="rounded-lg border border-red-300 bg-red-100 p-4 text-red-700">
-      {message}
-    </div>
-  );
-}
